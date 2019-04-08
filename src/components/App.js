88d598610ff7c23a94c3b65b8bd2cd174562a5ae
Bryan Leanoe <bryan.leano@gmail.com>
2019-04-07 19:54:46 -0600

    fetching through jservice

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
 
 class App extends React.Component {
+  componentDidMount() {
+    fetch('http://jservice.io/api/categories?count=20')
+      .then(response => response.json())
+      .then(json => console.log(json));
+  }
+
   render() {
     return (
       <div>
