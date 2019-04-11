18ad0cf80a6a4f8ed7a8afd0c7381aa4151db767
Bryan Leanoe <bryan.leano@gmail.com>
2019-04-10 22:03:23 -0600

    add catergory data through redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { setCategories } from '../actions';
+import { setCategories, pickCategory } from '../actions';
 
 class App extends React.Component {
   componentDidMount() {
@@ -22,7 +22,12 @@ class App extends React.Component {
           this.props.categories.map(category => {
             return (
               <div key={category.id}>
-                <Link to='category'><h4>{category.title}</h4></Link>
+                <Link 
+                  to='category'
+                  onClick={() => this.props.pickCategory(category)}
+                >
+                  <h4>{category.title}</h4>
+                </Link>
               </div>
             )
           })
@@ -33,7 +38,7 @@ class App extends React.Component {
 }
 
 function mapStateToProps(state) {
-  return { categories: state }
+  return { categories: state.categories }
 }
 
-export default connect(mapStateToProps, { setCategories })(App);
+export default connect(mapStateToProps, { setCategories, pickCategory })(App);
--- src/components/Category.js
+++ src/components/Category.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 class Category extends React.Component {
   render() {
+    console.log('this.props category', this.props)
+
     return (
       <div>
         <Link className='link-home' to='/'><h4>Home</h4></Link>
@@ -12,4 +15,8 @@ class Category extends React.Component {
   }
 }
 
-export default Category;
+function mapStateToProps(state) {
+  return { category: state.category }
+}
+
+export default connect(mapStateToProps, null)(Category);
