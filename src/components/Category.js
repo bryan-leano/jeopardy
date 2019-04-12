ad86ffc393e5e20e33dc656de0b1269ec8e2f6eb
Bryan Leanoe <bryan.leano@gmail.com>
2019-04-12 04:55:21 -0600

    used api to fetch category data

--- src/components/Category.js
+++ src/components/Category.js
@@ -3,13 +3,33 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 class Category extends React.Component {
+  constructor() {
+    super();
+
+    this.state = { clues: [] };
+  }
+
+
+  componentDidMount() {
+    fetch(`http://jservice.io/api/clues?category=${this.props.category.id}`)
+      .then(response => response.json())
+      .then(json => this.setState({ clues: json }));
+  }
+
   render() {
     console.log('this.props category', this.props)
 
     return (
       <div>
         <Link className='link-home' to='/'><h4>Home</h4></Link>
-        <h2>Category title</h2>
+        <h2>{this.props.category.title}</h2>
+        {
+          this.state.clues.map(clue => {
+            return (
+              <div key={clue.id}>{clue.question}</div>
+            )
+          })
+        }
       </div>
     )
   }
